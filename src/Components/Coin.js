13345051be8b18e93fe77b48a0dd248b2d56e6f3
Mohammed Kalasha <109412459+Mohammedkalasha@users.noreply.github.com>
2026-02-12 13:10:27 +0300

    Update Coin.js

--- src/Components/Coin.js
+++ src/Components/Coin.js
@@ -17,17 +17,9 @@ const Coin = ({
       <div className="coinRow">
         <div className="coinData">
           <div className="coin">
-            {/* <img src={icon} />
-            <h1 className="coinName">{coinName}</h1>
-            <p className="coinSymbol">{coinSymbol}</p>
-            <p className="coinPrice">$ {price.toFixed(2)}</p>
-            {priceChange < 0 ? (
-              <p className="priceChange red">{priceChange.toFixed(2)}%</p>
-            ) : (
-              <p className="priceChange green">{priceChange.toFixed(2)}%</p>
-            )}       */}
+    
 
-            <img src={icon} />
+            <img src={icon} alt={`${coin.name} logo` />
             <h1 className="coinName">{coinName}</h1>
             <p className="coinSymbol">{coinSymbol}</p>
 
