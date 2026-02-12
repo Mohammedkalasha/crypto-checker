bf047540d9acc491c68788ed5da118dc00cee52b
Mohammed Kalasha <109412459+Mohammedkalasha@users.noreply.github.com>
2026-02-12 11:53:59 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import "./App.css";
-import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import { BrowserRouter as Router, Route} from "react-router-dom";
 
 import Home from "./Routes/Home";
 import CoinPage from "./Routes/CoinPage";
